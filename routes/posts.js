236bfa145b4d1ebf6b104a7b835f9f133ac4e00c
Miguel Nogueira <miguel.nogueira@ua.pt>
2022-04-07 10:02:13 +0100

    get user info

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const User = require('../model/user');
 const verify = require('./verifytoken');
 
 router.get('/', verify, (req, res) => {
-    // res.send(req.user);    
+    res.send(req.user);    
 
     // Example of getting a single user id
     // User.findOne({_id: req.user});
