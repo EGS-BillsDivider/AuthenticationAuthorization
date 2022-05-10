b47a905b2421c998a18d7a2e309ebb98cc517f93
Miguel Nogueira <miguel.nogueira@ua.pt>
2022-05-10 15:45:06 +0100

    fixed interface and connection with DB working

--- index.js
+++ index.js
@@ -12,11 +12,23 @@ dotenv.config();
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECT, () => console.log('connected to DB!'));
 
+app.set('view-engine', 'ejs')
+
 //Middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false})); // para poder usar req.body.name quando escrever nos inputs da pag
+
+app.get('/register', (req,res) => {
+    res.render('register.ejs')
+});
+
+app.get('/login', (req,res) => {
+    res.render('login.ejs')
+});
+
 //Route middleware
 app.use('/', authRoute);
 app.use('/posts', postRoute);
 
 
-app.listen(3000, () => console.log("Server running!"));
+app.listen(8000, () => console.log("Server running!"));
